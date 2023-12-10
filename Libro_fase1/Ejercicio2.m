5c47a43362939bc90b63bb6b39170bf1dffd2d68
HarveyXocop <107698009+HarveyXocop@users.noreply.github.com>
2023-12-10 00:27:18 -0600

    Delete Libro_fase1 directory

--- Libro_fase1/Ejercicio2.m
+++
@@ -1,9 +0,0 @@
-%Calcular la temperatura media (o promedio)
-%y graficar los datos de temeparatura.
-%  
-tiempo = [0.0, 0.5, 1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0, 4.5, 5.0];
-temps = [105, 126, 119, 129, 132, 128, 131, 135, 136, 132, 137];
-promedio = mean(temps)
-plot (tiempo, temps), title('Mediciones de Temperatura' );...
-    xlabel ('Tiempo, minutos');...
-    ylabel ('Temperatura, Grados F'), grid 
--- Libro_fase1/readme.md
+++
@@ -1,7 +0,0 @@
-El problema No.1 del libro es el fichero Ejercicio2
-
-El problema No.2 del libro es el fichero Ejercicio6
-
-El problema No.3 del libro es el fichero Ejercicio12
-
-El problema No.4 del libro es el fichero Ejercicio15
